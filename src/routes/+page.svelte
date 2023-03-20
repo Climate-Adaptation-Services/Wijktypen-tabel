67cbe51631dbfc1eaf6b5c3be2fe23af8b6eca5d
stichtingcas <daniel@climateadaptationservices.com>
2023-03-20 12:07:42 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,59 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+  const wijken = ['Vernieuwd', 'Bedrijven-terrein', 'Villawijk',  'Vinex-wijk', 'Bloemkool-wijk', 'Hoogbouw', 'Tuinstad hoogbouw', 'Tuinstad laagbouw', 'Naoorlogse woonwijk', 'Vooroorlogse woonwijk', 'Tuindorp', 'Volkswijk', 'Stedelijk bouwblok', 'Historische binnenstad']
+  const groenPercentages = ['15', '15', '15', '15', '15', '15', '15', '15', '15', '15', '15', '15', '15', '15']
+
+</script>
+
+<table>
+  <tr>
+    <th>leeg</th>
+    {#each wijken as wijk, i}
+      <th>{wijk}</th>
+    {/each}
+  </tr>
+  <tr>
+    <td>plaatje</td>
+    {#each groenPercentages as perc, i}
+      <td>{perc}</td>
+    {/each}
+  </tr>
+  <tr>
+    <td>plaatje</td>
+    {#each groenPercentages as perc, i}
+      <td>{perc}</td>
+    {/each}
+  </tr>
+  <tr>
+    <td>plaatje</td>
+    {#each groenPercentages as perc, i}
+      <td>{perc}</td>
+    {/each}
+  </tr>
+  <tr>
+    <td>plaatje</td>
+    {#each groenPercentages as perc, i}
+      <td>{perc}</td>
+    {/each}
+  </tr>
+  <tr>
+    <td>plaatje</td>
+    {#each groenPercentages as perc, i}
+      <td>{perc}</td>
+    {/each}
+  </tr>
+</table>
+
+<style>
+  table{
+    width:98vw;
+    height:97vh;
+    table-layout: fixed;
+    border-spacing: 3px 3px;
+  }
+
+  td{
+    background-color: #eeeeef;
+  }
+
+</style>
