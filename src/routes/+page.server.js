c439077d294516c8d983040dabc2b0276c24b3be
stichtingcas <daniel@climateadaptationservices.com>
2023-09-28 09:53:45 +0200

    Update +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,7 @@
 import { dsv } from 'd3'
 
 export function load(){
-  const data = dsv(';', "https://gist.githubusercontent.com/stichtingcas/1debcfcdee82f11b6875ed2f3acbe94d/raw/729e7aad23354e8e1981ae295a7f1473241ac716/wijktypentabel.csv")
+  const data = dsv(';', "https://gist.githubusercontent.com/stichtingcas/1debcfcdee82f11b6875ed2f3acbe94d/raw/d56457671dce98b290e8eafadf386be065979eba/wijktypentabel.csv")
   return { data }
 }
 
